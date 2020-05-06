ce362e67ba215e8aa2af5f132e5d5641124926bd
Dolly Desir <dolly.desir@gmail.com>
2020-05-06 19:06:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def join_nested_strings(src)
    # src[count].is_a? (String) 
     # if src[count].is_a? (String)
     while src[row_index][count] < src[row_index][count].length do
-      src[0]
+      src[0][0]
       
       messageof_strings.push(String)
       count+= 1
